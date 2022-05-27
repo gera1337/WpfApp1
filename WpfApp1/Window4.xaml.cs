221d2da589246a7b83883296fd3ef2534c783322
neprognus <neprognus@gmail.com>
2022-05-27 11:31:10 +0300

    third commit

--- WpfApp1/Window4.xaml.cs
+++ WpfApp1/Window4.xaml.cs
@@ -23,5 +23,43 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void change_Click(object sender, RoutedEventArgs e)
+        {
+            string name = title.Text.Trim();
+            string desc = description.text.trim();
+            int.price=price.int.trim();
+            string.status=status.Text.Trim();
+            if (title.Length < 5)
+            {
+                title.ToolTip = "минимальная длинна логина 5 символов";
+                title.Background = Brushes.Red;
+            }
+            else if (description.Length < 5)
+            {
+                description.ToolTip = "минимальная длинна пароля 5 символов";
+                description.Background = Brushes.Red;
+            }
+            else if (price.Length < 2)
+            {
+                price.ToolTip = "минимальная длинна пароля 5 символов";
+                price.Background = Brushes.Red;
+            }
+            else if (status.Length < 5)
+            {
+                status.ToolTip = "минимальная длинна пароля 5 символов";
+                status.Background = Brushes.Red;
+            }
+            else
+            {
+                
+                db.users.change(user);
+                db.SaveChanges();
+
+
+            }
+
+
+        }
     }
 }
